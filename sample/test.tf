c442cdfbdf672961c926193db7649086de59f3d4
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-02-24 12:58:52 +0530

    updating vpc syntax

--- sample/test.tf
+++ sample/test.tf
@@ -8,5 +8,5 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
-    vpc_security_group_ids = [sg-061d282e835a9954b]
+    vpc_security_group_ids = ["sg-061d282e835a9954b"]
 }
