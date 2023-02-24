723be7fbbf0dd178ca22335a7f0b09165491230a
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-02-24 13:59:26 +0530

    updated code

--- sample/test.tf
+++ sample/test.tf
@@ -1,10 +1,3 @@
-# launch an EC2 instance
-#pre-req
-#  EC2 instance name = test
-#  which AMI - centos8
-#  instance type - t3.micro
-#  Networking settings - Security group - allow-all
-
 resource "aws_instance" "ec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
