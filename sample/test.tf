bce2b06ff74d7aaca15ef6467cea6d5a92ec1945
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-02-24 12:54:08 +0530

    creating EC2 instace

---
+++ sample/test.tf
@@ -0,0 +1,12 @@
+# launch an EC2 instance
+#pre-req
+#  EC2 instance name = test
+#  which AMI - centos8
+#  instance type - t3.micro
+#  Networking settings - Security group - allow-all
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0a017d8ceb274537d"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [sg-061d282e835a9954b]
+}
