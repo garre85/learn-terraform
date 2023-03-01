f87d4b04834ec3040fd1d67c60eb6a5513d0e239
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-03-01 12:33:23 +0530

    updated SG details

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -9,7 +9,7 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
-    vpc_security_group_ids = ["sg-061d282e835a9954b"]
+    vpc_security_group_ids = ["sg-0c31eb5809d4e24cc"]
     tags = {
         Name = "test"
     }
