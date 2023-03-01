2d9d6bfc32a56a53a5d374fa6125aa61d7ad13b0
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-03-01 12:06:53 +0530

    launching ec2 instance

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,3 +1,11 @@
+#Aganda to provision EC2 instance
+#Define resource of the service of AWS
+#<BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
+# Block body
+#  <IDENTIFIER> =  <EXPRESSION> #Argument
+#}
+
+
 resource "aws_instance" "ec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
