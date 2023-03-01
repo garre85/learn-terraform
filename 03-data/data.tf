851702fcb74bd28351b70f2226800bbc91a702ba
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-03-01 13:55:27 +0530

    getting ami_id output

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,11 +1,11 @@
 #data source , which will take resource details from attributes reference and through output as Argument reference.
 
-data "aws_ami" "ami"{
+data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["207019219082"]
 }
 
 output "ami_id" {
-  value = "data.aws_ami.ami.image_id"
+  value = data.aws_ami.ami.image_id
 }
