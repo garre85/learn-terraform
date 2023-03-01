fbe2e8ada6fb0b779872c0faf882d20ab8b986df
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-03-01 13:58:47 +0530

    owner details updated

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -3,7 +3,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["207019219082"]
+  owners           = ["973714476881"]
 }
 
 output "ami_id" {
