fead8d6e15e882d1c46c348b5ed5a164d486341a
Praveen Kumar <gvs.praveenkumar85@gmail.com>
2023-03-06 12:39:27 +0530

    variable types

--- 04-varaiable/var.tf
+++ 04-varaiable/var.tf
@@ -7,3 +7,27 @@ variable "sample"{
 output "sample" {
   value = var.sample
 }
+
+
+#Default variable type
+variable "sample1"{
+  default = "Hello sample1"
+}
+
+#List variable type
+variable "sample2" {
+  default = [
+     "Hello sample2",
+      100,
+      true
+  ]
+}
+
+#Map variable type
+variable "sample3" {
+  default = {
+    string ="Hello sample3"
+    number = 1000
+    boolean= true
+  }
+}
